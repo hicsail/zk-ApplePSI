913bc024e1dca7344d3c94d5cef4d56094cc7079
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2023-05-11 09:21:53 -0400

    V0 (#11)
    * Cuckoo Table implemented
    
    * Pederson hash and exponentiation
    
    * curvepoint

--- examples/apple_psi.py
+++ examples/apple_psi.py
@@ -2,53 +2,10 @@ from picozk import *
 import ecdsa
 from random import randrange
 from ecdsa import numbertheory
-from dataclasses import dataclass
 import sys
 sys.path.insert(1, './utils')
 from cuckoo_table import CuckooTable
-
-# TODO: v0 Move this to utils
-@dataclass
-class CurvePoint:
-    is_infinity: BooleanWire
-    x: ArithmeticWire
-    y: ArithmeticWire
-    p: int
-
-    # Mux for a curve point
-    def mux(self, cond, other):
-        return CurvePoint(mux_bool(cond, self.is_infinity, other.is_infinity),
-                          mux(cond, self.x, other.x),
-                          mux(cond, self.y, other.y), self.p)
-
-    # Point doubling
-    def double(self):
-        a = 0
-        l = ((3*(self.x*self.x) + a) * modular_inverse(2*self.y, self.p)) % self.p
-
-        x3 = (l * l) - (2 * self.x)
-        y3 = (l * (self.x - x3) - self.y)
-        return CurvePoint(self.is_infinity, x3 % self.p, y3 % self.p, self.p)
-
-    # Point addition
-    def add(self, other):
-        assert isinstance(other, CurvePoint)
-        assert val_of(self.is_infinity) == False
-        assert val_of(self.x) != val_of(other.x) or val_of(self.y) != val_of(other.y)
-        l = ((other.y - self.y) * modular_inverse(other.x - self.x, self.p)) % self.p
-        x3 = l*l - self.x - other.x
-        y3 = l * (self.x - x3) - self.y
-        return self.mux(other.is_infinity, CurvePoint(False, x3 % self.p, y3 % self.p, self.p))
-
-    # Point scaling by a scalar via repeated doubling
-    def scale(self, s):
-        bits = s.to_binary()
-        res = CurvePoint(True, 0, 0, self.p)
-        temp = self
-        for b in reversed(bits.wires):
-            res = temp.add(res).mux(b.to_bool(), res)
-            temp = temp.double()
-        return res
+from curvepoint import CurvePoint
 
 # Verify the ECDSA signature represented by (r, s)
 def verify(r, s, hash_val, pubkey, p):
@@ -129,4 +86,4 @@ print("non_empty_indices after", cuckoo_table.get_non_empty_indices())
 # TODO: v1 Make bots by polynomial interpolation
 
 
-# TODO: v1 verify cuckoo table process
+# TODO: v2 verify cuckoo table process
