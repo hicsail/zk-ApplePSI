634ecf675f7fff41d4be56d956fe4bab3064b33b
Harunobu Ishii <harunobuishii547@gmail.com>
2023-05-12 11:44:00 -0400

    restructuring

--- examples/example_pandas_dataframe.py
+++
@@ -1,8 +0,0 @@
-from picozk import *
-
-with PicoZKCompiler('picozk_test'):
-    df = pd.DataFrame(np.random.randint(0,100,size=(50000, 4)), columns=list('ABCD'))
-    sdf = df.applymap(SecretInt)
-    output = sdf['A'].sum()
-    print(output)
-    reveal(output)
