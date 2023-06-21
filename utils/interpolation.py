0fbbc5f3e916978691dd78568f101089ad271afc
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2023-06-21 15:32:48 -0400

    Update interpolation.py

--- utils/interpolation.py
+++ utils/interpolation.py
@@ -14,16 +14,3 @@ def lagrange_interpolation(points:list[CurvePoint], x, p):
         result = result.add(term)
 
     return result
-
-# def lagrange_interpolation(points:list[CurvePoint], x, p):
-#     n = len(points)
-#     result = CurvePoint(False, x, 0, p)
-
-#     for i in range(n):
-#         term = points[i][1].y
-#         for j in range(n):
-#             if j != i:
-#                 term *= ((x - points[j][1].x) * modular_inverse(points[i][1].x - points[j][1].x, p)) % p
-#         result.y = (result.y + term) % p
-
-#     return result
