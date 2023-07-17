58feec0423f1cf3818a825be20065d3fbdb1f235
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2023-07-17 16:25:13 -0400

    Test Created (#27)
    * naming
    
    * e2e test
    
    * e2e
    
    * e2e

--- utils/pedersen_hash.py
+++ utils/pedersen_hash.py
@@ -1,33 +1,4 @@
 import copy
-# def pedersen_hash(M, Points, p):
-
-#     num_bits = M.bit_length()
-    
-#     # Split M into two halves, a and b
-#     a = M >> (num_bits // 2)
-#     b = M & ((1 << (num_bits // 2)) - 1)
-
-#     # Splitting input into high and low bits
-#     a_low = a & ((1 << 248) - 1)
-#     a_high = a >> 248
-#     b_low = b & ((1 << 248) - 1)
-#     b_high = b >> 248
-
-#     # Calculate the sum
-#     result_x = Points[0].x()
-#     result_y = Points[0].y()
-#     result_x = result_x + a_low * Points[1].x()
-#     result_x = result_x + a_high * Points[2].x()
-#     result_x = result_x + b_low * Points[3].x()
-#     result_x = result_x + b_high * Points[4].x()
-
-#     result_y = result_y + a_low * Points[1].y()
-#     result_y = result_y + a_high * Points[2].y()
-#     result_y = result_y + b_low * Points[3].y()
-#     result_y = result_y + b_high * Points[4].y()
-
-#     # Return the resulting group point 
-#     return result_x, result_y
 
 def pedersen_hash(M, Points, p):
 
