69d4133742735ccb8295d664fcf2eec4becbfedd
Harunobu Ishii <harunobuishii547@gmail.com>
2023-07-25 16:55:37 -0400

    WIP: Opt lagrange

--- utils/interpolation.py
+++ utils/interpolation.py
@@ -14,21 +14,13 @@ def lagrange_coefficients(ai, bi, roots, p):
 
     for k in range(n):
         sigma = sym_polynomial(n-1-k, [root for root in roots if root != ai]) % p
-        print("sigma", sigma)
-        print("(-1)**(n-1-k)", (-1)**(n-1-k))
-        print("modular_inverse(int(np.prod([ai - ak for ak in roots if ak != ai])), p)", modular_inverse(int(np.prod([ai - ak for ak in roots if ak != ai])), p))
-        # a = ((-1)**(n-1-k) * sigma * modular_inverse(int(np.prod([ai - ak for ak in roots if ak != ai]) % p), p)) % p
-        print("(sigma * modular_inverse(int(np.prod([ai - ak for ak in roots if ak != ai]), p)) % p)", (sigma * modular_inverse(int(np.prod([ai - ak for ak in roots if ak != ai]) % p), p)) % p)
-        a = ((-1)**(n-1-k) * (sigma * modular_inverse(int(np.prod([ai - ak for ak in roots if ak != ai]) % p), p)) % p) % p
-        print("a", a)
-        c_k = bi.scale(int(a))
+        num  = (-1)**(n-1-k) * sigma % p
+        denom = modular_inverse(np.prod([ai - ak for ak in roots if ak != ai]) % p, p) % p
+        term = num * denom % p
+        c_k = bi.scale(int(term))
         c_k_values.append(c_k)
-        print("c_k", c_k)
-        print("")
 
     c_k_values.reverse()
-    print("c_k_values", c_k_values)
-    print("")
 
     return c_k_values
 
@@ -49,19 +41,17 @@ def lagrange_poly(xs, ys, p):
             if s == None:
                 s = c[i]
             else:
-                s.add(c[i])
+                s = s.add(c[i])
         coeffs.append(s)
-
     return coeffs
     
 def calc_polynomial(x, coeffs):
 
     res = None
 
-    for i, a in enumerate(reversed(coeffs)):
-
+    for i, coeff in enumerate(reversed(coeffs)):
         if res == None:
-            res = a.scale(x**i)
+            res = coeff.scale(x**i)
         else:
-            res.add(a.scale(x**i))
+            res = res.add(coeff.scale(x**i))
     return res
