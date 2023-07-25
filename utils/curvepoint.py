9fcbdd096562899005280f55ec0fcfef4dc9324f
Harunobu Ishii <harunobuishii547@gmail.com>
2023-07-25 14:40:32 -0400

    Assertion for identical point removed

--- utils/curvepoint.py
+++ utils/curvepoint.py
@@ -27,7 +27,7 @@ class CurvePoint:
     # Point addition
     def add(self, other):
         assert isinstance(other, CurvePoint)
-        assert val_of(self.x) != val_of(other.x) or val_of(self.y) != val_of(other.y)
+        # assert val_of(self.x) != val_of(other.x) or val_of(self.y) != val_of(other.y)
         l = ((other.y - self.y) * modular_inverse(other.x - self.x, self.p)) % self.p
         x3 = l*l - self.x - other.x
         y3 = l * (self.x - x3) - self.y
