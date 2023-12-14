8a378fd222c4ec3ba625e30eb5462c16608db2c2
Harunobu Ishii <harunobuishii547@gmail.com>
2023-12-14 12:55:40 +0900

    calc_lag old added bacl

--- apple_psi/interpolation.py
+++ apple_psi/interpolation.py
@@ -59,6 +59,40 @@ def calc_lagrange_terms_bary(xs, ys, cuckoo_table, p):
     return lagrange_bases, n - 1
 
 
+def calc_denominators(xs, n, p):
+    denominators = []
+    for i in range(n):
+        denominator = 1
+        for j in range(n):
+            if j != i:
+                denominator *= modular_inverse(xs[i] - xs[j], p)
+        denominators.append(denominator)
+    return denominators
+
+
+def calc_lagrange_terms(xs, ys, cuckoo_table, p):
+    assert len(xs) == len(ys)
+    n = len(xs)
+
+    # Preprocessing Lgrange Polynomial (Only terms)
+    lagrange_bases = {}  # idx in cuckoo table (key): terms (value)
+
+    denominators = calc_denominators(xs, n, p)
+
+    # Calculate terms for all index in the cuckoo table
+    for idx, _ in enumerate(cuckoo_table.table):
+        terms = []
+        for i in range(n):
+            term = ys[i]
+            numerator = 1
+            for j in range(n):
+                if j != i:
+                    numerator *= idx - xs[j]
+            term = term.scale(numerator * denominators[i] % p)
+            terms.append(term)
+        lagrange_bases[idx] = terms
+    return lagrange_bases
+
 
 def calc_polynomial(idx, lagrange_bases):
     result = None
